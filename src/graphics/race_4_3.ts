08ae0a981621b5ecd316de4def73ed65fa77f282
Tempystral <elijah.doern@gmail.com>
2023-05-19 12:46:36 -0400

    Import assets, create final pages

---
+++ src/graphics/race_4_3.ts
@@ -0,0 +1,8 @@
+import { createHead } from '@vueuse/head';
+import { createApp } from 'vue';
+import ActiveRunPage from './pages/ActiveRunPage.vue';
+
+const app = createApp(ActiveRunPage, { players: 2, ratio: '4-3' });
+const head = createHead();
+app.use(head);
+app.mount('#app');
---
+++ src/graphics/solo_GBA.ts
@@ -0,0 +1,8 @@
+import { createHead } from '@vueuse/head';
+import { createApp } from 'vue';
+import ActiveRunPage from './pages/ActiveRunPage.vue';
+
+const app = createApp(ActiveRunPage, { players: 1, ratio: '3-2' });
+const head = createHead();
+app.use(head);
+app.mount('#app');
