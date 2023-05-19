feac4ae03c021cc86b51983cec4ed9ba97320c72
Tempystral <elijah.doern@gmail.com>
2023-05-19 17:56:41 -0400

    Add fontawesome support

--- src/graphics/race_4p.ts
+++
@@ -1,8 +0,0 @@
-import { createHead } from '@vueuse/head';
-import { createApp } from 'vue';
-import ActiveRunPage from './pages/ActiveRunPage.vue';
-
-const app = createApp(ActiveRunPage);
-const head = createHead();
-app.use(head);
-app.mount('#app');
--- src/graphics/solo_16_9.ts
+++ src/graphics/solo_16_9.ts
@@ -1,8 +1,16 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTwitch } from '@fortawesome/free-brands-svg-icons';
+import { faGamepad } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { createHead } from '@vueuse/head';
 import { createApp } from 'vue';
 import ActiveRunPage from './pages/ActiveRunPage.vue';
 
+library.add(faTwitch);
+library.add(faGamepad);
+
 const app = createApp(ActiveRunPage, { players: 1, ratio: '16-9' });
 const head = createHead();
 app.use(head);
+app.component('font-awesome-icon', FontAwesomeIcon);
 app.mount('#app');
