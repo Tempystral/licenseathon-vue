13d58a1ae2f94bc44c0ba31dbafe5b41e8d0329d
Tempystral <elijah.doern@gmail.com>
2025-04-17 14:51:21 -0400

    Add nodecg dependency

---
+++ cfg/nodecg.js
@@ -0,0 +1,12 @@
+module.exports = {
+    host: '0.0.0.0',
+    port: 9090,
+    logging: {
+        console: {
+            enabled: true,
+            timestamps: false,
+            level: 'verbose',
+            replicants: false,
+        },
+    },
+};
