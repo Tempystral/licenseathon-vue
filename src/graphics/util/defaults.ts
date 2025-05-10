3298a611ef36ffb5eba9e5c44a276b6909ce1c87
Tempystral <elijah.doern@gmail.com>
2025-05-10 14:57:47 -0400

    Update types

--- src/graphics/util/defaults.ts
+++ src/graphics/util/defaults.ts
@@ -1,38 +1,45 @@
-export const defaultRunDataPlayer = {
-  name: '',
-  id: '',
-  teamID: '',
-  country: '',
-  pronouns: '',
+import {
+  RunData,
+  RunDataPlayer,
+  RunDataTeam,
+  Timer,
+} from "speedcontrol-util/types";
+
+export const defaultRunDataPlayer: RunDataPlayer = {
+  name: "",
+  id: "",
+  teamID: "",
+  country: "",
+  pronouns: "",
   social: {
-    twitch: '',
+    twitch: "",
   },
-  customData: { },
+  customData: {},
 };
 
-export const defaultRunDataTeam = {
-  name: '',
-  id: '',
+export const defaultRunDataTeam: RunDataTeam = {
+  name: "",
+  id: "",
   players: [],
 };
 
-export const defaultRunData = {
+export const defaultRunData: RunData = {
   teams: [],
   customData: {},
-  id: '',
+  id: "",
 };
 
-export const defaultTimer = {
-  time: '00:00:00',
-  state: 'stopped',
+export const defaultTimer: Timer = {
+  time: "00:00:00",
+  state: "stopped",
   milliseconds: 0,
   timestamp: 0,
   teamFinishTimes: {},
 };
 
 export const defaultTeamFinishTime = {
-  time: '00:00:00',
-  state: 'stopped',
+  time: "00:00:00",
+  state: "stopped",
   milliseconds: 0,
   timestamp: 0,
 };
