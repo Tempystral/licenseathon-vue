84d210982aafacd219974e63622926a39fcd3a00
Tempystral <elijah.doern@gmail.com>
2025-06-01 20:40:01 -0400

    Make page creation template take props

--- src/graphics/solo_4_3.ts
+++ src/graphics/solo_4_3.ts
@@ -1,17 +1,4 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faTwitch } from '@fortawesome/free-brands-svg-icons';
-import { faGamepad, faMicrophone } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { createHead } from '@vueuse/head';
-import { createApp } from 'vue';
-import ActiveRunPage from './pages/ActiveRunPage.vue';
+import { create } from "@licenseathon-vue/browser-shared/template";
+import ActiveRunPage from "./pages/ActiveRunPage.vue";
 
-library.add(faMicrophone);
-library.add(faTwitch);
-library.add(faGamepad);
-
-const app = createApp(ActiveRunPage, { players: 1, ratio: '4-3' });
-const head = createHead();
-app.use(head);
-app.component('font-awesome-icon', FontAwesomeIcon);
-app.mount('#app');
+const app = create(ActiveRunPage, { players: 1, ratio: "4-3" });
