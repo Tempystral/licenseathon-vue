9a96e74d77a8b14019b47f5f054b48c12154477b
Tempystral <12647331+Tempystral@users.noreply.github.com>
2026-05-25 12:08:57 -0400

    Made LCD wrapper more reusable

---
+++ src/graphics/components/panels/LCDItem.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = defineProps<{
+  label: string;
+}>();
+</script>
+<template>
+  <div class="lcd-item min-w-20 h-full flex flex-col items-center justify-end">
+    <slot />
+    <label class="absolute -bottom-4 text-lcns-label text-xs">
+      {{ label }}
+    </label>
+  </div>
+</template>
+<style>
+.lcd-item > figure {
+  margin-bottom: -0.25rem;
+}
+</style>
--- src/graphics/pages/BlankPage.vue
+++ src/graphics/pages/BlankPage.vue
@@ -8,9 +8,12 @@ import {
 import InsetContainer from "../components/InsetContainer.vue";
 import TextBox from "../components/text/TextBox.vue";
 import TextLabel from "../components/text/TextLabel.vue";
-import LCDPanel from "../components/LCDPanel.vue";
 import SvgIcon from "@jamescoyle/vue-icon";
-import MaterialPanel from "../components/MaterialPanel.vue";
+import LCDPanel from "../components/panels/LCDPanel.vue";
+import MaterialPanel from "../components/panels/MaterialPanel.vue";
+import TimerComponent from "../components/TimerComponent.vue";
+import LCDPanel2 from "../components/panels/LCDPanel2.vue";
+import LCDItem from "../components/panels/LCDItem.vue";
 
 const timerTestData = [
   {
@@ -35,14 +38,14 @@ const timerTestData = [
     <TextBox
       theme="nameplate"
       :icon="mdiGamepad"
-      class="absolute top-40 left-40 h-15 w-120"
+      class="absolute top-10 left-10 h-15 w-120"
     >
       Donkey Kong Country Returns: Tropical Freeze
     </TextBox>
     <TextBox
       theme="pronouns"
       :icon="mdiGamepad"
-      class="absolute top-60 left-40 h-12 w-80"
+      class="absolute top-30 left-10 h-12 w-80"
     >
       They/Them
     </TextBox>
@@ -50,13 +53,19 @@ const timerTestData = [
     <TextBox
       theme="lcd"
       :icon="mdiGamepad"
-      class="absolute top-80 left-40 h-13 w-120"
+      class="absolute top-50 left-10 h-13 w-120"
     >
       Donkey Kong Country Returns: Tropical Freeze
     </TextBox>
 
+    <TimerComponent
+      :players="1"
+      ratio="16-9"
+      class="top-100 left-10 text-black"
+    ></TimerComponent>
+
     <div class="absolute top-40 left-200 w-150 flex flex-wrap">
-      <MaterialPanel theme="amber" class="w-fit">
+      <MaterialPanel theme="amber" :connects="['bl']" class="w-fit">
         <LCDPanel class="w-fit" :items="timerTestData">
           <template #item="{ contents, label, icon }">
             <div
@@ -65,36 +74,58 @@ const timerTestData = [
               <div v-if="icon" class="grow">
                 <SvgIcon type="mdi" :path="icon" :size="40" class="h-full" />
               </div>
-              <div :class="label === 'TIMER' ? 'text-7xl' : ''">
+              <div :class="label === 'TIMER' ? 'text-7xl' : 'text-md -mb-1'">
                 {{ contents }}
               </div>
-              <div class="absolute -bottom-4 text-gray-600 text-xs">
+              <div class="absolute -bottom-4 text-lcns-label text-xs">
                 {{ label }}
               </div>
             </div>
           </template>
         </LCDPanel>
       </MaterialPanel>
-      <div
-        class="grow min-w-3 bg-lcns-amber inner-corner-xl corner-bl mb-3"
-      ></div>
+      <div class="grow bg-lcns-amber inner-corner-xl corner-bl mb-3"></div>
       <MaterialPanel theme="amber" class="grow">
         <!-- <div class="bg-lcns-white rounded-bl-xl grow -mt-3 -mr-3">a</div> Corner element concept -->
         <InsetContainer theme="amber" :align="'right'">
-          <TextLabel text="PLATFORM" position="top" align="start">
-            <TextBox theme="nameplate" :icon="mdiGamepad" class="h-12">
+          <TextLabel text="PLATFORM" position="left" class="w-full h-15">
+            <TextBox theme="lcd">
               Donkey Kong Country Returns: Tropical Freeze
             </TextBox>
           </TextLabel>
 
           <TextLabel text="GAME TITLE" position="left" class="w-full h-15">
-            <TextBox theme="lcd" :icon="mdiGamepad" class="h-full">
-              DOOM (2016)
-            </TextBox>
+            <TextBox theme="lcd"> DOOM (2016) </TextBox>
           </TextLabel>
         </InsetContainer>
       </MaterialPanel>
     </div>
+
+    <LCDPanel2 class="w-fit absolute top-10 left-203">
+      <LCDItem label="TIMER">
+        <TimerComponent :players="1" ratio="16-9" class="text-7xl" />
+      </LCDItem>
+      <LCDItem label="PLATFORM">
+        <TextLabel
+          text="GAMECUBE"
+          position="bottom"
+          size="md"
+          class="text-lcns-teal"
+        >
+          <SvgIcon type="mdi" :path="mdiGamepadVariant" :size="40" />
+        </TextLabel>
+      </LCDItem>
+      <LCDItem label="YEAR">
+        <TextLabel
+          text="2001"
+          position="bottom"
+          size="md"
+          class="text-lcns-teal"
+        >
+          <SvgIcon type="mdi" :path="mdiCalendarMonth" :size="40" />
+        </TextLabel>
+      </LCDItem>
+    </LCDPanel2>
 
     <!-- Adjust the InsetContainer so it's ONLY the inset which manages its own margin/padding away from the edges
      of its parent. The parent will just be a material panel which you can slap the inset container and stuff onto -->
