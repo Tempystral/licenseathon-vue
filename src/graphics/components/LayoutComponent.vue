0430508d41fe15240709c1cbfd4095f667daed15
Tempystral <12647331+Tempystral@users.noreply.github.com>
2026-06-10 00:05:42 -0400

    Update layout component to calculate screen size manually

--- src/graphics/components/LayoutComponent.vue
+++ src/graphics/components/LayoutComponent.vue
@@ -1,12 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from "vue";
+
+const { mainWidth: width, aspectRatio = [16, 9] } = defineProps<{
+  mainWidth: number;
+  aspectRatio: [number, number];
+}>();
+
+const height = computed(() => (width * aspectRatio[1]) / aspectRatio[0]);
+
+const gridStyles = computed(
+  () => `
+  grid-template-columns: calc(100% - 8px - ${width}rem) ${width}rem;
+  grid-template-rows: ${height.value}rem calc(100% - 8px - ${height.value}rem)
+  `,
+);
+</script>
 
 <template>
-  <div class="absolute grid layout p-2 gap-2 h-full w-full">
+  <div class="absolute grid layout p-2 gap-2 h-full w-full" :style="gridStyles">
     <div v-if="$slots.left" id="left" style="grid-area: left">
       <slot name="left" />
     </div>
 
-    <div id="main" class="aspect-retro" style="grid-area: main">
+    <div id="main" style="grid-area: main">
       <slot name="main" />
     </div>
 
@@ -21,8 +37,8 @@
 @use "@licenseathon-vue/sass/color" as theme;
 
 .layout {
-  grid-template-columns: 1fr auto;
-  grid-template-rows: auto 1fr;
+  /* grid-template-columns: 1fr auto;
+  grid-template-rows: auto 1fr; */
   /* grid-template-areas:
     "left main"
     "foot foot"; */
