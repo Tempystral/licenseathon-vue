f1ab77c7fa2b491616abdc473c58bce08b7cd7ff
Tempystral <12647331+Tempystral@users.noreply.github.com>
2026-07-16 17:08:32 -0400

    Never mind this shit barely works anyways

--- vite.config.mjs
+++ vite.config.mjs
@@ -17,7 +17,7 @@ export default defineConfig({
     sourcemap: false,
   },
   plugins: [
-    vue({isProduction: false}),
+    vue(),
     //vueDevTools({ launchEditor: "code" }),
     tailwindcss(),
     checker({ vueTsc: { tsconfigPath: "tsconfig.browser.json" } }),
